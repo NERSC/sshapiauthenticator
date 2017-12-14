c68cc357eff9d56a2aa94ed3dcd749edd0877131
Shane Canon <scanon@lbl.gov>
2017-12-13 16:13:02 -0800

    Add missing self

--- sshapiauthenticator/auth.py
+++ sshapiauthenticator/auth.py
@@ -33,7 +33,7 @@ class SSHAPIAuthenticator(Authenticator):
             headers={'Authorization':'Basic %s:%s' % (username, pwd)}
             r = requests.post( self.server, headers=headers)
             if r.status_code==200:
-               file = '%s/%s.key' %(cert_path, username)
+               file = '%s/%s.key' %(self.cert_path, username)
                with open(file, 'w') as f:
                   f.write(r.text)
                os.chmod(file, 0o600)
